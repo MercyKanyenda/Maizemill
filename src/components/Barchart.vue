e965797187acd75a36c075be4bd8da959139483a
MercyKanyenda <merceekanyenda1@gmail.com>
2024-02-11 14:31:12 +0200

    BarChart and chartConfig

--- src/components/Barchart.vue
+++ src/components/Barchart.vue
@@ -1,26 +1,30 @@
-<script setup>
-import { Bar } from 'vue-chartjs'
-import { ref } from 'vue'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-const chartData = {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
-      }
-     const chartOptions = {
-        responsive: true
-      }
-</script>
 <template>
-    <div>
-<template>
-  <Bar
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-  />
-</template>
-    </div>
-</template>
+    <Bar :data="data" :options="options" />
+  </template>
+  
+  <script lang="ts">
+  import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale
+  } from 'chart.js'
+  import { Bar } from 'vue-chartjs'
+  import * as chartConfig from './chartConfig.js'
+  
+  ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+  
+  export default {
+    name: 'App',
+    components: {
+      Bar
+    },
+    data() {
+      return chartConfig
+    }
+  }
+  </script>
+  
